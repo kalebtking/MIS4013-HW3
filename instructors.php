c8bac6d33cd603e17f41d26b3050bdef1ec0bc67
kalebtking <kalebtking@icloud.com>
2024-10-02 10:30:57 -0500

    Update instructors.php

--- instructors.php
+++ instructors.php
@@ -6,7 +6,7 @@ require_once("model-instructors.php");
 $pageTitle = "Instructors";
 include "view-header.php";
 $instructors = selectInstructors();
-include "view-intructors.php";
+include "view-instructors.php";
 include "view-footer.php";
 
 ?>
