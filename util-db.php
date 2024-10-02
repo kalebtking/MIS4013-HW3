14b170e7b435e2f26ad8ad431a2c83e49ead37bc
kalebtking <kalebtking@icloud.com>
2024-10-02 11:14:29 -0500

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -1,16 +1,3 @@
-// <?php
-// function get_db_connection(){
-//     // Create connection
-//     $conn = new mysqli('hw3-mis4013.mysql.database.azure.com', 'kalebtking', '!Wolverine0', 'hw3');
-    
-//     // Check connection
-//     if ($conn->connect_error) {
-//       return false;
-//     }
-//     return $conn;
-// }
-// ?>
-
 <?php
 function get_db_connection(){
     // Create connection
@@ -18,9 +5,11 @@ function get_db_connection(){
     
     // Check connection
     if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+      return false;
     }
     return $conn;
 }
 ?>
+
+
 
