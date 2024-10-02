5a83376ff6291cc85b63b3cdcc5e5b1d6a62e065
kalebtking <kalebtking@icloud.com>
2024-10-02 10:38:51 -0500

    Update model-instructors.php

--- model-instructors.php
+++ model-instructors.php
@@ -4,6 +4,9 @@ function selectInstructors() {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT Instructor_ID, Instructor_Name, email FROM instructor");
         $stmt->execute();
+        if (!$stmt) {
+    die("Query preparation failed: " . $conn->error);
+}
         $result = $stmt->get_result();
         $conn->close();
         return $result;
