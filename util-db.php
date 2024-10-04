9126b06b11f924dfa500bfaa763382edb655be40
kalebtking <kalebtking@icloud.com>
2024-10-04 18:55:06 -0500

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -1,9 +1,7 @@
 <?php
 function get_db_connection(){
     // Create connection
-   // $conn = new mysqli('d09ee87442dd', 'kalebtking', '!Wolverine0', 'hw3');
-    $conn = mysqli_init();mysqli_ssl_set($conn,NULL,NULL, "/Users/kalebking/Desktop/MIS-4013", NULL, NULL);
-    mysqli_real_connect($conn, "hw3-mis4013.mysql.database.azure.com", "kalebtking", "!Wolverine0", "hw3", 3306, MYSQLI_CLIENT_SSL);
+   $conn = new mysqli('hw3-mis4013.mysql.database.azure.com', 'kalebtking', '!Wolverine0', 'hw3');
     
     // Check connection
     if ($conn->connect_error) {
