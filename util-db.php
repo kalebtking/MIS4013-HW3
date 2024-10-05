f105693c9902e2b6266bdb5b7273c7abf4cd667e
kalebtking <kalebtking@icloud.com>
2024-10-04 19:58:30 -0500

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -2,8 +2,8 @@
 function get_db_connection(){
     // Create connection
    //$conn = new mysqli('hw3-mis4013.mysql.database.azure.com', 'kalebtking', '!Wolverine0', 'hw3');
-    $con = mysqli_init();
-mysqli_ssl_set($con,NULL,NULL, "/Users/kalebking/Desktop/MIS-4013/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+    $conn = mysqli_init();
+mysqli_ssl_set($conn,NULL,NULL, "/Users/kalebking/Desktop/MIS-4013/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
 mysqli_real_connect($conn, "hw3-mis4013.mysql.database.azure.com", "kalebtking", "!Wolverine0", "hw3", 3306, MYSQLI_CLIENT_SSL);
     
     // Check connection
