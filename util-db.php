d075759004039c55071be9629467e928c92576b5
kalebtking <kalebtking@icloud.com>
2024-10-04 20:05:26 -0500

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -1,11 +1,15 @@
 <?php
-function get_db_connection(){
+function get_db_connection()
+{
     // Create connection
-   //$conn = new mysqli('hw3-mis4013.mysql.database.azure.com', 'kalebtking', '!Wolverine0', 'hw3');
+    //$conn = new mysqli('hw3-mis4013.mysql.database.azure.com', 'kalebtking', '!Wolverine0', 'hw3');
     $conn = mysqli_init();
-mysqli_ssl_set($conn,NULL,NULL, "/Users/kalebking/Desktop/MIS-4013/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-mysqli_real_connect($conn, "hw3-mis4013.mysql.database.azure.com", "kalebtking", "!Wolverine0", "hw3", 3306, MYSQLI_CLIENT_SSL);
-    
+    mysqli_ssl_set($conn, NULL, NULL, "/Users/kalebking/Desktop/MIS-4013/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+    mysqli_real_connect($conn, "hw3-mis4013.mysql.database.azure.com", "kalebtking", "!Wolverine0", "hw3", 3306, MYSQLI_CLIENT_SSL);
+    if (mysqli_connect_errno($conn)) {
+        die('Failed to connect to MySQL: ' . mysqli_connect_error());
+    }
+
     // Check connection
     // if ($conn->connect_error) {
     //   return false;
